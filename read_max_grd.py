84c3a30642bde2e12bc6cd6d2f817c7601b8545f
shubham-688 <agr.shubh.agr@gmail.com>
2025-06-13 15:45:45 -0400

    lat long range in read_max..

--- read_max_grd.py
+++ read_max_grd.py
@@ -6,13 +6,15 @@ directory = "2.5deg_grad_grds/"
 
 azimuths = range(-170, 190, 10)
 
+region = [-158, -142, 58, 69]
+
 vmins = []
 vmaxs = []
 
 # Loop over azimuth values and extract min and max from each GRD file
 for azimuth in azimuths:
     grdfile = f"{directory}grad_mask.{azimuth}.grd"
-    info = pygmt.grdinfo(grdfile, C=True).strip().split()
+    info = pygmt.grdinfo(grdfile, per_column=True,R=region).strip().split()
     vmin = float(info[4])
     vmax = float(info[5])
     vmins.append(vmin)
@@ -23,7 +25,11 @@ avg_vmin = np.mean(vmins)
 avg_vmax = np.mean(vmaxs)
 
 # Output the results
-print("Minimum values:", vmins)
+print("Minimum values:", vmins,'\n')
+print("Min of all values:", np.min(vmins))
+print('#######')
 print("Maximum values:", vmaxs)
+print("Max of all values:", np.max(vmaxs))
+
 print("Average minimum:", avg_vmin)
 print("Average maximum:", avg_vmax)
